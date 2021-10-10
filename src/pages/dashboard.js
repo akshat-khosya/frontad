c7a42da9f92442556d72c148832757d69675327c
akshat-khosya <20106@iiitu.ac.in>
2021-10-10 09:52:44 +0530

    oh

---
+++ src/pages/dashboard.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import Sidebar from './Sidebar';
+import Popup from '../components/Popup';
+function Dashboard(props){
+    function logout(){
+            props.currentLogin();
+    }
+    return(
+       
+        <>
+        <Popup />
+         <Sidebar /> 
+        
+        <h1 onClick={logout}>Logout</h1>
+       
+
+        </>
+    )
+}
+
+
+export default Dashboard;
